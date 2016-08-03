68c25e9d0628418ba8a4c5d8f5ee87e1a75dc38e
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-03 14:00:11 -0500

    accessing form elements

---
+++ app.js
@@ -0,0 +1,8 @@
+/****************************************************
+*              Accessing Form Elements              *
+****************************************************/
+var x = document.forms[0].elements[0].name;
+document.write(x);
+
+var y = document.bucysForm.username.name;
+document.write(y);
